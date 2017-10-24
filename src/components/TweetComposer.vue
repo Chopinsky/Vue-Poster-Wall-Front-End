883ad86c4b9af17844c5d6b0b8d263df0e62aaa1
Jacob Zuo <chopinsky@live.com>
2017-10-23 20:01:56 -0500

    upload photo icon

--- src/components/TweetComposer.vue
+++ src/components/TweetComposer.vue
@@ -3,22 +3,43 @@
     <div class="pv2 tc bb b--black-10">
       <h1 class="ma0 f5 normal">Compose New Tweet</h1>
     </div>
+
     <div class="bg-near-white pa3">
       <textarea name="tweet" v-model="tweet" rows="3"
                 class="w-100 br2 ba b--black-10 pa2"
                 placeholder="Write your tweet here"
       >
       </textarea>
-      <div class="tr">
-        <span class="mr3 black-70"
-              v-bind:class="{ 'light-red': underTwentyMark(), 'dark-red': underTenMark() }">
-          {{ charLeft() }}
-        </span>
-        <button :disabled="isTweetable()"
-                class="button-reset bg-blue bn white f6 fw5 pv2 ph3 br2 pointer dim"
-        >
-          Tweet
-        </button>
+
+      <div v-if="photoHasBeenUploaded()" class="bg-black-10 pa2 flex">
+        <figure class="ma0 relative flex items-center justify-center">
+          <button @click="removePhoto()" class="button-reset pointer dim bn bg-black h2 w2 br-100 white flex items-center justify-center absolute absolute--fill-l center">
+            <i class="material-icons f5">close</i>
+          </button>
+          <img v-bind:src="photo" class="h3 w3" alt="Uploaded photo">
+        </figure>
+      </div>
+
+      <input @change="handlePhotoUpload()" ref="photoUpload" type="file" class="hide">
+
+      <div class="mt3 flex justify-between">
+        <div>
+          <button @click="triggerFileUpload()" class="button-reset flex items-center br2 bn bg-transparent blue hover-bg-black-10 pointer">
+            <i class="material-icons f3">photo_camera</i>
+          </button>
+        </div>
+
+        <div class="flex items-center">
+          <span class="mr3 black-70"
+                v-bind:class="{ 'light-red': underTwentyMark(), 'dark-red': underTenMark() }">
+            {{ charLeft() }}
+          </span>
+          <button :disabled="isTweetable()"
+                  class="button-reset bg-blue bn white f6 fw5 pv2 ph3 br2 pointer dim"
+          >
+            Tweet
+          </button>
+        </div>
       </div>
     </div>
   </div>
@@ -46,6 +67,15 @@
       },
       underTenMark: function () {
         return (this.maxCharCount() - this.tweet.length < 10)
+      },
+      photoHasBeenUploaded: function () {
+        return false
+      },
+      handlePhotoUpload: function () {
+
+      },
+      triggerFileUpload: function () {
+
       }
     }
   }
@@ -60,5 +90,36 @@
   button[disabled] {
     cursor: not-allowed;
     opacity: 0.5;
+  }
+
+  .hide {
+    clip: rect(1px, 1px, 1px, 1px);
+    height: 1px;
+    overflow: hidden;
+    position: absolute !important;
+    width: 1px;
+  }
+
+  @font-face {
+    font-family: 'Material Icons';
+    font-style: normal;
+    font-weight: 400;
+    src: url(https://fonts.gstatic.com/s/materialicons/v30/2fcrYFNaTjcS6g4U3t-Y5ZjZjT5FdEJ140U2DJYC3mY.woff2) format('woff2');
+  }
+
+  .material-icons {
+    font-family: 'Material Icons';
+    font-weight: normal;
+    font-style: normal;
+    font-size: 24px;
+    line-height: 1;
+    letter-spacing: normal;
+    text-transform: none;
+    display: inline-block;
+    white-space: nowrap;
+    word-wrap: normal;
+    direction: ltr;
+    -webkit-font-feature-settings: 'liga';
+    -webkit-font-smoothing: antialiased;
   }
 </style>
