5d4d982ceca4220ad70f94468fd9a356ef90600a
Jacob Zuo <chopinsky@live.com>
2017-11-09 17:53:28 -0600

    adding text area

--- react_implementation/src/components/Composer.js
+++ react_implementation/src/components/Composer.js
@@ -14,6 +14,7 @@ export default class Composer extends Component {
     super(props);
 
     this.state = {
+      "tweet": "",
       "isOpen": true
     }
 
@@ -22,14 +23,23 @@ export default class Composer extends Component {
 
   initBinding() {
     this.getInnerAreaStyle = this.getInnerAreaStyle.bind(this);
+    this.handleToggle = this.handleToggle.bind(this);
+    this.handleTweet = this.handleTweet.bind(this);
   }
 
-  toggleModal(e) {
-    e.stopPropagation();
-    e.preventDefault();
+  handleToggle(event) {
+    event.stopPropagation();
+    event.preventDefault();
 
     let { isOpen } = this.state;
     this.setState({ "isOpen": !isOpen });
+  }
+
+  handleTweet(event) {    
+    event.stopPropagation();
+    event.preventDefault();
+
+    this.setState({ "tweet": event.target.value });
   }
 
   getInnerAreaStyle() {
@@ -41,11 +51,15 @@ export default class Composer extends Component {
   render() {
     return (
       <div id="divContainer" style={containerStyle} className="w-75 center ba b--black-10">
-        <div style={bannerStyle} className="pv2 tc bb b--black-10 hover-effect" onClick={ this.toggleModal.bind(this) }>
+        <div style={bannerStyle} className="pv2 tc bb b--black-10 hover-effect" onClick={this.handleToggle}>
           <h1 className="ma0 f5 normal">Write On The Wall</h1>
         </div>
 
-        <div style={ this.getInnerAreaStyle() } >
+        <div style={ this.getInnerAreaStyle() } className="bg-near-white pa3" >
+          <textarea id="tweet" name="tweet" rows="3"
+                    className="w-100 br2 ba b--black-10 pa2"
+                    value={this.state.tweet} onChange={this.handleTweet}
+                    placeholder="Write your thoughts here" />
           Composer Body
         </div>
       </div>
